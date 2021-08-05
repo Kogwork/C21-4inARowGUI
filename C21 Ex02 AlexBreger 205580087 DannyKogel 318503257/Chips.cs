62801c3cc146b5c35359febc52fda54966824245
Alex <alexbreger123@gmail.com>
2021-08-05 15:34:19 +0300

    Add project files.

---
+++ C21 Ex02 AlexBreger 205580087 DannyKogel 318503257/Chips.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C21_Ex02_AlexBreger_205580087_DannyKogel_318503257
+{
+    class Chips
+    {
+    }
+}
---
+++ C21 Ex02 AlexBreger 205580087 DannyKogel 318503257/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C21_Ex02_AlexBreger_205580087_DannyKogel_318503257
+{
+    class Program
+    {
+        //Board Class
+        //Chip Class
+        // Initiate GamePlay method (taht indicates if the game is on) ('Q' stops the game and gives a point to the oponnent)
+        // Intiate board size method
+        // PVE or PVP game (if pve, user starts)
+        // Print board method
+        // Insert into board method
+        // Check if column full method
+        // Check if win condition is true (WIN OR TIE)
+        // Initiate player's turn method?
+        // If game ends, ask for another round. If another round, continue with same board and score.
+
+    }
+}
